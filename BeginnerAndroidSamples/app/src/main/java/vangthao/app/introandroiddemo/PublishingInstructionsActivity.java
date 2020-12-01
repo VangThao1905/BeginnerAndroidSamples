b0ffb3dca1eb5403b3ddc90a388637d2f2872fe1
VangThao1905 <vangthao1905@gmail.com>
2020-12-01 15:24:40 +0700

    Added Chapter 9, Chapter 10 and contents in it

---
+++ BeginnerAndroidSamples/app/src/main/java/vangthao/app/introandroiddemo/PublishingInstructionsActivity.java
@@ -0,0 +1,14 @@
+package vangthao.app.introandroiddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublishingInstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publishing_instructions);
+    }
+}
